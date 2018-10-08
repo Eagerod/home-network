b61924e60885c1bf52c9ada7712fa0cea5e6d49d
Aleem Haji <hajial@gmail.com>
2018-10-08 02:38:49 -0400

    Updated plex mounts to be a bit more resilient to failure.

--- .scripts/mount_disks.py
+++ .scripts/mount_disks.py
@@ -4,11 +4,30 @@ import sys
 import yaml
 
 
+def print_shell_command(args):
+    print ' '.join([a if ' ' not in a else '"{}"'.format(a) for a in args])
+
+
 with open(sys.argv[1]) as f:
     volumes_yaml = yaml.load(f)
 
 for volume in volumes_yaml.get('volumes', {}):
-    params = [
+    mkdir_params = ['mkdir', '-p', volume['local']]
+    print_shell_command(mkdir_params)
+
+    # mkdir will fail if the directory to be mounted to already exists. Ignore
+    #   failures.
+    proc = subprocess.Popen(mkdir_params)
+    proc.wait()
+
+    umount_params = ['umount', volume['local']]
+    print_shell_command(umount_params)
+    # The unmount will fail if there's nothing already mounted there, so don't
+    #   bother checking the returncode on this call.
+    proc = subprocess.Popen(umount_params)
+    proc.wait()
+
+    mount_params = [
         'mount',
         '-t', volume['type'],
         volume['remote'],
@@ -16,23 +35,11 @@ for volume in volumes_yaml.get('volumes', {}):
     ]
 
     if 'extras' in volume:
-        params.insert(3, '-o')
-        params.insert(4, volume['extras'])
+        mount_params.insert(3, '-o')
+        mount_params.insert(4, volume['extras'])
 
-    proc = subprocess.Popen(['mkdir', '-p', volume['local']])
-    proc.wait()
-
-    if proc.returncode != 0:
-        print >> sys.stderr, 'Failed to create directory in {}'.format(volume['local'])
-        sys.exit(-1)
-
-    # The unmount will fail if there's nothing already mounted there, so don't
-    #   bother checking the returncode on this call.
-    proc = subprocess.Popen(['umount', volume['local']])
-    proc.wait()
-
-    print ' '.join([p if ' ' not in p else '"{}"'.format(p) for p in params])
-    proc = subprocess.Popen(params)
+    print_shell_command(mount_params)
+    proc = subprocess.Popen(mount_params)
     proc.wait()
 
     if proc.returncode != 0:
