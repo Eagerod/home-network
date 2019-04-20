eb5db50d4c97e45431ffb7c55815eda82683d284
Aleem Haji <hajial@gmail.com>
2019-04-19 22:37:40 -0400

    Added in some error handling for slack bot.

--- slack-bot/bot.py
+++ slack-bot/bot.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from flask import Flask, request
@@ -6,15 +7,34 @@ from slackclient import SlackClient
 # Simple application that will listen on a port, and will forward on messages
 #   to the provided Slack account.
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 slack_client = SlackClient(os.environ['SLACK_API_KEY'])
 default_channel = os.environ['DEFAULT_CHANNEL']
 
+
 @app.route('/message', methods=['POST'])
 def receive_internal_message():
     message_body = request.get_data()
+    if not message_body:
+        return 'No message content', 400
+
     channel = request.headers.get('X-SLACK-CHANNEL-ID') or default_channel
 
-    slack_client.api_call('chat.postMessage', channel=channel, text=message_body)
+    logging.info
+    rv = slack_client.api_call('chat.postMessage',
+                               channel=channel, text=message_body)
+
+    if not rv['ok']:
+        # See if this is the server, or the sender's fault.
+        if rv['error'] == 'invalid_auth':
+            logging.error('Slack API is incorrectly configured.')
+            return '', 500
+
+        error = 'Unknown error from Slack: {}. Blaming the client.'.format(
+            rv['error']
+        )
+        logging.warning(error)
+        return rv['error'], 400
 
     return '', 200
