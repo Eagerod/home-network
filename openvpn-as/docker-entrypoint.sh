4a722a0e4dbee5cd1b633536d95a000dbb61a2f1
Aleem Haji <hajial@gmail.com>
2019-05-03 00:24:34 -0400

    Finall found a reliable way to eliminate users from openvpn-as in a somewhat reliable way.

--- openvpn-as/docker-entrypoint.sh
+++ openvpn-as/docker-entrypoint.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 #
 # Force the removal of several users, and modify some other configurations.
-#
-# The first time the container is started, there are a lot of configurations
-#   that haven't been set up yet, so all of these commands will fail. Because
-#   of that, `set -e` isn't included in here.
+
+CONFIG_FILE=/config/etc/as.conf
 
 # # Delete users that likely exist in the user database
 /usr/local/openvpn_as/scripts/sacli --user admin UserPropDelAll
 /usr/local/openvpn_as/scripts/sacli --user openvpn UserPropDelAll
 
-# Disable login for users that 
-sed -i 's/^boot_pam_service=openvpnas/# boot_pam_service=openvpnas/' /config/etc/as.conf
-sed -i -r 's/^(boot_pam_users.*)/# \1/' /config/etc/as.conf
+# Disable login for PAM users
+sed -i -r 's/^(boot_pam_service.*)/# \1/g' $CONFIG_FILE
+sed -i -r 's/^(boot_pam_users.*)/# \1/g' $CONFIG_FILE
 
-# Remove users from the container that shouldn't be in there.
-userdel admin
-userdel openvpn
+echo "admin:$OPENVPN_PRIMARY_USERPASS" | chpasswd
 
 exec /init
