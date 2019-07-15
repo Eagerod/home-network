9cc5c32b1481702b0895c3596c9ef117230ffa62
Aleem Haji <hajial@gmail.com>
2019-07-14 21:41:21 -0400

    Added in collection skipping for mongodb backup.

--- mongodb/mongodb-backup.sh
+++ mongodb/mongodb-backup.sh
@@ -7,11 +7,18 @@ if [ -z "${MONGODB_HOST}" ]; then
 fi
 
 mongo --host ${MONGODB_HOST} --quiet --eval 'db.getMongo().getDBNames().join("\n");' | while read database; do
+    set -e 
+
     if [ "$database" = "admin" ] || [ "$database" = "local" ] || [ "$database" = "config" ] || [ "$database" = "unifi_stat" ]; then
         echo >&2 "Skipping database $database"
         continue
     fi
 
     echo >&2 "Backing up database: $database"
-    mongodump --host ${MONGODB_HOST} --db "$database" -o "/var/lib/backup/$database"
+    if [ "$database" = "remind-me-bot" ]; then
+        echo >&2 "Skipping logs collection"
+        mongodump --quiet --host ${MONGODB_HOST} --db "$database" --excludeCollection=logs -o "/var/lib/backups/$database"
+    else
+        mongodump --quiet --host ${MONGODB_HOST} --db "$database" -o "/var/lib/backups/$database"
+    fi
 done
