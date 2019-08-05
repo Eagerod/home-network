3fb9be850272925575c483016b9a59f58f2a3b61
Aleem Haji <hajial@gmail.com>
2019-08-05 13:52:57 -0400

    Updated certbot script to be a little more explicit with everything, and work with the lighter linux distro the scripts run in.

--- certbot/update-secrets.sh
+++ certbot/update-secrets.sh
@@ -17,13 +17,13 @@ replace_certificates() {
         exit 1
     fi
 
-    cert_name=$1
-    cert_path=$2
+    cert_name="$1"
+    cert_path="$2"
 
-    rsa_keyfile=$(mktemp).rsa.key
+    rsa_keyfile="$(mktemp).rsa.key"
 
     echo "Updating $cert_name..."
-    openssl rsa -in $(find $cert_path -iname "privkey*.pem" | sort -n | tail -1) -out $rsa_keyfile
+    openssl rsa -in $(find "$cert_path" -iname "privkey*.pem" | sort -n | tail -1) -out "$rsa_keyfile"
     curl \
         -fsSL \
         --insecure \
@@ -33,12 +33,12 @@ replace_certificates() {
         -d '
         {
             "data":{
-                "tls.crt": "'$(base64 -w 0 $(find $cert_path -iname "fullchain*.pem" | sort -n | tail -1))'",
-                "tls.key": "'$(base64 -w 0 $(find $cert_path -iname "privkey*.pem" | sort -n | tail -1))'",
-                "tls.rsa.key": "'$(base64 -w 0 $rsa_keyfile)'"
+                "tls.crt": "'$(base64 $(find "$cert_path" -iname "fullchain*.pem" | sort -n | tail -1) | tr -d '\n')'",
+                "tls.key": "'$(base64 $(find "$cert_path" -iname "privkey*.pem" | sort -n | tail -1) | tr -d '\n')'",
+                "tls.rsa.key": "'$(base64 "$rsa_keyfile" | tr -d '\n')'"
             }
         }' \
-        https://$KUBERNETES_SERVICE_HOST/api/v1/namespaces/default/secrets/$cert_name > /dev/null
+        "https://$KUBERNETES_SERVICE_HOST/api/v1/namespaces/default/secrets/$cert_name" > /dev/null
 
     rm $rsa_keyfile
 }
@@ -49,15 +49,15 @@ replace_combined_certificate() {
         exit 1
     fi
 
-    cert_name=$1
-    cert_path=$2
+    cert_name="$1"
+    cert_path="$2"
 
-    combined_file=$(mktemp).pem
+    combined_file="$(mktemp).pem"
 
     echo "Updating $cert_name..."
     cat \
-        $(find $cert_path -iname "fullchain*.pem" | sort -n | tail -1) \
-        $(find $cert_path -iname "privkey*.pem" | sort -n | tail -1) > $combined_file
+        $(find "$cert_path" -iname "fullchain*.pem" | sort -n | tail -1) \
+        $(find "$cert_path" -iname "privkey*.pem" | sort -n | tail -1) > $combined_file
     curl \
         -fsSL \
         --insecure \
@@ -67,15 +67,15 @@ replace_combined_certificate() {
         -d '
         {
             "data":{
-                "keycert.pem": "'$(base64 -w 0 $combined_file)'"
+                "keycert.pem": "'$(base64 "$combined_file" | tr -d '\n')'"
             }
         }' \
-        https://$KUBERNETES_SERVICE_HOST/api/v1/namespaces/default/secrets/$cert_name > /dev/null
+        "https://$KUBERNETES_SERVICE_HOST/api/v1/namespaces/default/secrets/$cert_name" > /dev/null
 
     rm $combined_file
 }
 
-replace_certificates $INTERNAL_WILDCARD_SECRET_NAME $INTERNAL_WILDCARD_ARCHIVE
-replace_combined_certificate $INTERNAL_WILDCARD_COMBINED_SECRET_NAME $INTERNAL_WILDCARD_ARCHIVE
-replace_certificates $WILDCARD_SECRET_NAME $WILDCARD_ARCHIVE
-replace_combined_certificate $WILDCARD_COMBINED_SECRET_NAME $WILDCARD_ARCHIVE
+replace_certificates "$INTERNAL_WILDCARD_SECRET_NAME" "$INTERNAL_WILDCARD_ARCHIVE"
+replace_combined_certificate "$INTERNAL_WILDCARD_COMBINED_SECRET_NAME" "$INTERNAL_WILDCARD_ARCHIVE"
+replace_certificates "$WILDCARD_SECRET_NAME" "$WILDCARD_ARCHIVE"
+replace_combined_certificate "$WILDCARD_COMBINED_SECRET_NAME" "$WILDCARD_ARCHIVE"
