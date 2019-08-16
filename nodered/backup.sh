78d5bbc038b33e8e6fdc2c405926a96a2095e482
Aleem Haji <hajial@gmail.com>
2019-08-15 21:46:46 -0400

    Add node-red cron.

--- nodered/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Copy the flows json to the backup dir.
-
-set -e
-
-docker cp $(docker ps -q --filter name=node-red):/root/.node-red/flows_node-red.json /var/lib/backups/node-red/
