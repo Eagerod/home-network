dd22efc45baf2f43971f690cb7f40156df29aec3
Aleem Haji <hajial@gmail.com>
2023-02-18 11:16:22 -0500

    Adds in an optional 'incomplete downloads' dir for transmission

--- transmission/docker-entrypoint.sh
+++ transmission/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -euf
 
 if [ -z "$PEER_PORT" ]; then
     echo >&2 "Must define \$PEER_PORT."
@@ -7,9 +8,15 @@ fi
 
 CONFIG_DIR=/var/lib/transmission-daemon/info
 WATCH_DIR=/var/lib/transmission-daemon/watch
+INCOMPLETE_DIR=/var/lib/incomplete-downloads
 
 mkdir -p "$CONFIG_DIR" "$WATCH_DIR"
 
 sed 's/"peer-port".*/"peer-port": '"$PEER_PORT"',/' /tmp/settings.json > "$CONFIG_DIR/settings.json"
 
-exec transmission-daemon -f --config-dir "$CONFIG_DIR" --watch-dir "$WATCH_DIR"
+if [ -d "$INCOMPLETE_DIR" ]; then
+    echo "Starting transmission with incomplete dir."
+    exec transmission-daemon -f --config-dir "$CONFIG_DIR" --watch-dir "$WATCH_DIR" --incomplete-dir "$INCOMPLETE_DIR"
+else
+    exec transmission-daemon -f --config-dir "$CONFIG_DIR" --watch-dir "$WATCH_DIR"
+fi
