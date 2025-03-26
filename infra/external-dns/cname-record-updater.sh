1bdcadbb4f9381d3ab592bece2ab438b9017db74
Aleem Haji <hajial@gmail.com>
2025-03-26 01:41:08 -0400

    Sets up the cname manaager to run on the cluster

--- infra/external-dns/cname-record-updater.sh
+++ infra/external-dns/cname-record-updater.sh
@@ -100,9 +100,9 @@ trap 'rm -f "$dbfile"' EXIT
 sqlite3 "$dbfile" 'CREATE TABLE records (domain TEXT PRIMARY KEY, response TEXT);'
 
 kubectl get ingress --output-watch-events -A -w -o custom-columns="NAMESPACE:.object.metadata.namespace,NAME:.object.metadata.name,INGRESS_CLASS:.object.spec.ingressClassName,HOSTS:.object.spec.rules[*].host,EVENT:.type" | \
-  awk "{ if (\$3 == \"$INGRESS_CLASS\") print; fflush()}" | \
-  awk "{ if (\$4 != \"$CNAME_TARGET\") print; fflush()}" | \
-  awk '{ print $1, $2, $4, $5; fflush() }' | \
+  awk -Winteractive "{ if (\$3 == \"$INGRESS_CLASS\") print }" | \
+  awk -Winteractive "{ if (\$4 != \"$CNAME_TARGET\") print }" | \
+  awk -Winteractive '{ print $1, $2, $4, $5 }' | \
   while read -r line; do
 	resource_update "$line"
   done
