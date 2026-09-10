0834444b1ac207675bb4fb3ab56da44512a82850
Aleem Haji <hajial@gmail.com>
2026-09-09 22:16:56 -0400

    Ignore 3 shellcheck infos

--- infra/namespace/list-failed-jobs-script.sh
+++ infra/namespace/list-failed-jobs-script.sh
@@ -7,6 +7,7 @@ SLACK_URL="https://slackbot.internal.aleemhaji.com/message"
 
 FAILED_JOB_JSONPATH='{range .items[?(@.status.failed>0)]}{.metadata.name}{"\n"}{end}'
 FAILED_JOB_FORMAT='kubectl -n %s delete job %s\n'
+# shellcheck disable=SC2016
 FULL_MESSAGE_FORMAT='Namespace "%s" has failed jobs:\n```\n%s\n```'
 
 slack() {
@@ -28,7 +29,9 @@ while true; do
 	failed_jobs="$(kubectl -n "$namespace" get jobs -o "jsonpath=$FAILED_JOB_JSONPATH")"
 
 	if [ -n "$failed_jobs" ]; then
+		# shellcheck disable=SC2059
 		fj_indent="$(while read -r line; do printf "$FAILED_JOB_FORMAT" "$namespace" "$line"; done <<< "$failed_jobs")"
+		# shellcheck disable=SC2059
 		fail_msg="$(printf "$FULL_MESSAGE_FORMAT" "$namespace" "$fj_indent")"
 		echo >&2 "$fail_msg"
 		slack "$fail_msg"
